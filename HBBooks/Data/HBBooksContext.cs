a570a5a35b6c2f39d4703b1a10685100b0390f0d
Hân <hanhamho963@gmail.com>
2023-06-21 09:53:54 +0700

    first commit

---
+++ HBBooks/Data/HBBooksContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HBBooks.Models;
+
+    public class HBBooksContext : DbContext
+    {
+        public HBBooksContext (DbContextOptions<HBBooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HBBooks.Models.Book> Book { get; set; } = default!;
+    }
---
+++ HBBooks/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace HBBooks.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Description { get; set; }
+
+
+        public int? Price { get; set; }
+    }
+}
